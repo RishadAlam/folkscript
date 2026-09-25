8fd2277c449966c9088a74a653d65316a34d78cc
Rishad Alam <sazzadullalamrishad@gmail.com>
2026-09-25 16:25:11 +0600

    Remove unused assets and Laravel starter scaffolding

--- routes/console.php
+++ routes/console.php
@@ -1,15 +1,10 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 use App\Models\Post;
 use App\Models\User;
 use App\Jobs\SendWeeklyDigest;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 Artisan::command('posts:publish-scheduled', function () {
     $count = 0;
